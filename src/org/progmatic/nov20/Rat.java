59d15131204edf86c0e87be443899863514d41bb
aronjt <jtelekiaron@gmail.com>
2020-11-20 13:25:09 +0100

    Prison

---
+++ src/org/progmatic/nov20/Rat.java
@@ -0,0 +1,33 @@
+package org.progmatic.nov20;
+
+public class Rat extends Prisoner {
+
+    public Rat(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean confess(Prisoner prisoner) {
+        return true;
+    }
+
+    @Override
+    public int setAges(int age) {
+        return ages+=age;
+    }
+
+    @Override
+    public int getAges() {
+        return ages;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getEnemies() {
+        return enemies.size();
+    }
+}
---
+++ src/org/progmatic/nov20/Strategy.java
@@ -0,0 +1,5 @@
+package org.progmatic.nov20;
+
+public interface Strategy {
+    boolean confess(Prisoner prisoner);
+}
